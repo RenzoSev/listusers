c8dc414e923f923554c0b01c7f4a17ca2f0055c6
Renzo Sevilha <73757018+RenzoSev@users.noreply.github.com>
2024-04-11 00:12:51 -0300

    tests/fix: add missing slash

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -77,7 +77,7 @@ describe('List Users', () => {
   });
 
   it('should open modal with correct address info', () => {
-    cy.visit('http://localhost:5173/debug');
+    cy.visit('http://localhost:5173/debug/');
     cy.get('dialog').should('have.css', 'opacity', '0');
     cy.get('dialog').should('have.css', 'visibility', 'visible');
     cy.get('dialog').should('have.css', 'pointer-events', 'none');
