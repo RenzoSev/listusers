d0bae004b5de694587852f5e7e55e7bafb6c3f79
Renzo Sevilha <73757018+RenzoSev@users.noreply.github.com>
2024-04-11 00:53:56 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import istanbul from 'vite-plugin-istanbul';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://renzosev.github.io/listusers/',
+  base: './',
   plugins: [
     react(),
     istanbul({
